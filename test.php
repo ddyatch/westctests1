6232cd6ad2cadec3fd2576cddc04afab3f0d551c
ddyatch <72565471+ddyatch@users.noreply.github.com>
2023-08-24 04:29:50 +0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Document</title>
+	<script src="//api.bitrix24.com/api/v1/"></script>
+	<!--JQuery-->
+	<script src="assets/js/jquery.js"></script>
+	<!--Bootstrap js-->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+	<!-- Bootstrap CSS-->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<!-- Fontawesome CSS -->
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="assets/css/accordion.css">
+</head>
+<body>
+<p>Тест</p>
+</body>
+</html>
